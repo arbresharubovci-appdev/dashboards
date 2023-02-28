a23492dd71b14c45caa70424d7f9dc2d25e33856
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-02-28 23:19:12 +0000

    second currency done

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,9 +2,22 @@ class CurrenciesController < ApplicationController
   def first_currency
     @raw_date = open("https://api.exchangerate.host/symbols").read
     @parsed_data = JSON.parse(@raw_date)
-    #@array_of_symbols
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
     render({:template => "currency_template/step_one.html.erb"})
   end
+  
+  def second_currency
+    @raw_date = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_date)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
 
+    #params are
+    # Parameters: {"from_currency"=>"ARS"}
+    @from_symbol = params.fetch("from_currency")
+
+    render({:template => "currency_template/step_two.html.erb"})
+  end
 
 end
